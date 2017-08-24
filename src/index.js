9dc1edf7a630ea856c2830b4760fb076b578638a
Kenneth Powers <ken@kenpowers.net>
2017-08-24 15:39:37 -0400

    Initial implementation and tests.

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-export default 'foo';
+import { forEach, isObject, assign, camelCase } from 'lodash';
+
+const nested = (input, ...keys) => {
+  const obj = {};
+  forEach(input, (value, key) => {
+    if (isObject(value)) {
+      assign(obj, nested(value, ...keys, key));
+    } else {
+      obj[camelCase(`${[...keys, key].join('-')}`)] = value;
+    }
+  });
+  return obj;
+};
+
+export default nested;
--- src/index.test.js
+++ src/index.test.js
@@ -1,6 +1,68 @@
 import test from 'ava';
-import foo from './';
+import nested from './';
 
 test('sanity check', (t) => {
-  t.is(foo, 'foo');
+  t.is(typeof nested, 'function');
+});
+
+test('single level', (t) => {
+  t.deepEqual(nested({
+    font: {
+      family: 'Lato',
+      size: '1em',
+    },
+  }), {
+    fontFamily: 'Lato',
+    fontSize: '1em',
+  });
+});
+
+test('recursive levels', (t) => {
+  t.deepEqual(nested({
+    border: {
+      style: 'dashed',
+      top: {
+        width: '1px',
+        color: 'black',
+      },
+      bottom: {
+        width: '2px',
+        color: 'red',
+      },
+    },
+  }), {
+    borderStyle: 'dashed',
+    borderTopWidth: '1px',
+    borderTopColor: 'black',
+    borderBottomWidth: '2px',
+    borderBottomColor: 'red',
+  });
+});
+
+test('multiple recursive levels', (t) => {
+  t.deepEqual(nested({
+    border: {
+      style: 'dashed',
+      top: {
+        width: '1px',
+        color: 'black',
+      },
+      bottom: {
+        width: '2px',
+        color: 'red',
+      },
+    },
+    margin: {
+      top: 10,
+      bottom: 5,
+    },
+  }), {
+    borderStyle: 'dashed',
+    borderTopWidth: '1px',
+    borderTopColor: 'black',
+    borderBottomWidth: '2px',
+    borderBottomColor: 'red',
+    marginTop: 10,
+    marginBottom: 5,
+  });
 });
