72948ba44dec85cbed653cb612e213973e236ce2
Kenneth Powers <ken@kenpowers.net>
2017-08-24 18:50:10 -0400

    Add composition test.

--- src/index.test.js
+++ src/index.test.js
@@ -1,5 +1,6 @@
 import test from 'ava';
 import { css } from 'glamor';
+import { compose } from 'lodash/fp';
 import nested from './';
 
 test('sanity check', (t) => {
@@ -119,6 +120,37 @@ test('with glamor (no object-rest-spread)', (t) => {
       bottom: 5,
     },
   })), css({
+    background: 'green',
+    borderStyle: 'dashed',
+    borderTopWidth: '1px',
+    borderTopColor: 'black',
+    borderBottomWidth: '2px',
+    borderBottomColor: 'red',
+    marginTop: 10,
+    marginBottom: 5,
+  }));
+});
+
+test('with glamor (composed)', (t) => {
+  const ncss = compose(css, nested);
+  t.deepEqual(ncss(nested({
+    background: 'green',
+    border: {
+      style: 'dashed',
+      top: {
+        width: '1px',
+        color: 'black',
+      },
+      bottom: {
+        width: '2px',
+        color: 'red',
+      },
+    },
+    margin: {
+      top: 10,
+      bottom: 5,
+    },
+  })), css({ // Match without composition
     background: 'green',
     borderStyle: 'dashed',
     borderTopWidth: '1px',
